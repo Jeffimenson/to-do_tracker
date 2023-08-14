18f471dc566bd4fc3fcd3e3c2b0b7759ead0b470
Jeffimenson <jeffengim@gmail.com>
2023-08-14 11:16:47 -0400

    Make quest group class operational

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ div.textContent = "hello world";
 class Task {
     description;
     isOptional;
-    completionDate = null;
+    #completionDate = null;
     constructor (description, isOptional){
         this.description = description;
         this.isOptional = isOptional;
     }
 
-    complete(){
-        this.completionDate = "TODAY'S DATE";
+    trackCompletionDate(){
+        this.#completionDate = "TODAY'S DATE";
     }
 
 }
@@ -31,8 +31,9 @@ class Quest {
     #tasks = [];
     #completedTasks = [];
 
-    constructor (name, dueDate=null){
+    constructor (name, tasks=[], dueDate=null){
         this.name = name; 
+        this.#tasks = tasks;
         this.dueDate = dueDate;
     }
 
@@ -43,14 +44,63 @@ class Quest {
     addTask(task){
         this.#tasks.unshift(task);
     }
+
+    completeTask(index){
+        const task = this.#tasks[index];
+        task.trackCompletionDate();
+        this.#completedTasks.unshift(task);
+        this.removeTask(index);
+    }
+    
+    shouldBeComplete(){
+        return this.#tasks.length === 0 && this.#completedTasks > 0;
+    }
+    trackCompletionDate(){
+        this.#completionDate = "TODAY'S DATE";
+    }
 }
 
 class QuestGroup {
-    quests = [];
-    completedQuests = []; 
+    #quests = [];
+    #completedQuests = []; 
 
     constructor(){}
-
     
+    addQuest(name, tasks){
+        const newQuest = new Quest(name, tasks);
+        this.#quests.push(newQuest);
+    }
+
+    removeQuest(index){
+        this.#quests.splice(index, 1);
+    }
+
+    swapQuests(i1, i2){
+        const temp = this.#quests[i1];
+        this.#quests[i1] = this.#quests[i2];
+        this.#quests[i2] = temp;
+    }
+
+    getQuest(index){
+        return this.#quests[index];
+    }
+
+    completeQuest(index){
+        const quest = this.#quests[index];
+        quest.trackCompletionDate();
+        this.#completedQuests.push(quest);
+        this.removeQuest(index);
+    }
     
 }
+
+const testGroup = new QuestGroup();
+const tasks = [];
+for (let i = 0; i < 10; i++){
+    const newTask = new Task("Activate lever " + i, false);
+    tasks.push(newTask);
+}
+const testQuest = new Quest("Lever mania", tasks);
+testGroup.addQuest(testQuest);
+testGroup.removeQuest(0);
+console.log(testGroup);
